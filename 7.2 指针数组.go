4157f436472ef73067353a6d0a828f128bb7d686
Xutangxin <8554884+xu-tangxin@user.noreply.gitee.com>
2021-06-10 20:56:01 +0800

    2nd commit

--- 7.2 指针数组.go
+++ 7.2 指针数组.go
@@ -4,18 +4,17 @@ import "fmt"
 
 func main() {
 
-	// arr := [3]int{1, 2, 3}
-	// //声明了一个指针数组
-	// var pointer [3]*int
-	// //循环 为指针数组赋值
-	// for i := 0; i < 3; i++ {
-	// 	pointer[i] = &arr[i]
-	// }
+	arr := [3]int{1, 2, 3}
+	//声明了一个指针数组
+	var pointer [3]*int
+	//循环 为指针数组赋值
+	for i := 0; i < 3; i++ {
+		pointer[i] = &arr[i]
+	}
 
-	// fmt.Println(pointer) //[0xc000016018 0xc000016020 0xc000016028]
+	fmt.Println(pointer) //[0xc000016018 0xc000016020 0xc000016028]
 
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Printf("arr[%d]=%d\n", i, *pointer[i])
-	// }
-	fmt.Println("hello world")
+	for i := 0; i < 3; i++ {
+		fmt.Printf("arr[%d]=%d\n", i, *pointer[i])
+	}
 }
