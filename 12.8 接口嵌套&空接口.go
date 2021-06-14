8f972d10de33daddc47124185bd4e244617b7a1b
Xutangxin <8554884+xu-tangxin@user.noreply.gitee.com>
2021-06-14 15:36:30 +0800

    update

--- 12.8 接口嵌套&空接口.go
+++ 12.8 接口嵌套&空接口.go
@@ -31,6 +31,21 @@ func show(a string) {
 	fmt.Println(a)
 }
 
+//类型断言 判断空接口类型函数
+func justifyType(x interface{}) {
+	switch v := x.(type) {
+	case string:
+		fmt.Println("the type of x is string, the value is ", v)
+	case int:
+		fmt.Println("the type of x is int, the value is ", v)
+	case bool:
+		fmt.Println("the type of x is bool, the value is ", v)
+	default:
+		fmt.Println("unsupport type", v)
+	}
+
+}
+
 //空接口作为函数参数
 //空接口可以接受任何类型的参数
 func showData(a interface{}) {
@@ -48,6 +63,15 @@ func main() {
 	x = "hello"
 	x = false
 	fmt.Println(x)
+	//类型断言，判断空接口存储类型的值
+	v, ok := x.(bool)
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("类型断言失败")
+	}
+
+	justifyType(x)
 
 	test := "fsociety"
 	test2 := []int{1, 2, 3, 4, 5}
